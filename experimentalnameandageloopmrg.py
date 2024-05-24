116f0979aaac4c41121db300c3558f7b8f2b11b5
MRGonzalez1990 <maurogonzalezplus@gmail.com>
2024-05-23 18:45:10 -0700

    Add files via upload

---
+++ experimentalnameandageloopmrg.py
@@ -0,0 +1,14 @@
+#Author Mauro Ruben Gonzalez
+x=input('What is your age?')
+if int(x) >= 18:
+    print('Adult')
+elif int(x)<18:
+        print('Underage')
+y=input('What is your name?')
+print(y)
+if y== ('mauro'):
+    print (y+' Good job!')
+elif y== ('ruben'):
+    print (y+' Good work!')
+else:
+    print (y=input('What is your name?'))
